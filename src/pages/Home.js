eb6a2751916747aac6c41c52d909362bfd3219bc
PTC-TCOM02\divine.sackitey <divine.sackitey@petraonline.com>
2023-07-09 23:27:51 +0000

    worked on the services list section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,7 @@ import Helmet from '../components/Helmet/Helmet'
 import { Container, Row, Col } from 'reactstrap'
 import FindCarForm from '../components/UI/FindCarForm'
 import AboutSection from '../components/UI/AboutSection'
+import ServicesList from '../components/UI/ServicesList'
 
 const Home = () => {
   return (
@@ -26,6 +27,17 @@ const Home = () => {
         </div>
       </section>
       <AboutSection />
+      <section>
+        <Container>
+          <Row>
+            <Col lg='12' className='mb-5 text-center'>
+              <h6 className='section__subtitle'>See our</h6>
+              <h2 className='section__title'>Popular Services</h2>
+            </Col>
+            <ServicesList />
+          </Row>
+        </Container>
+      </section>
     </Helmet>
   )
 }
