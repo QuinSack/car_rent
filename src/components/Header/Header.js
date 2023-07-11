f0afad2c76747248095a186d95d5078c4b9e4cdd
PTC-TCOM02\divine.sackitey <divine.sackitey@petraonline.com>
2023-07-11 15:26:40 +0000

    worked on the responsiveness of the header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useRef} from 'react'
 import { Container, Row, Col } from 'reactstrap'
 import { Link, NavLink } from 'react-router-dom'
 import '../../styles/header.css'
@@ -27,6 +27,8 @@ const navLinks = [
 ]
 
 const Header = () => {
+    const menuRef = useRef(null)
+    const toggleMenu = () => menuRef.current.classList.toggle('menu__active')
   return (
     <header className='header'>
         <div className='header__top'>
@@ -87,9 +89,9 @@ const Header = () => {
             <Container>
                 <div className='navigation__wrapper d-flex align-items-center justify-content-between'>
                     <span className='mobile__menu'>
-                        <i class="ri-menu-line"></i>
+                        <i class="ri-menu-line" onClick={toggleMenu}></i>
                     </span>
-                    <div className='navigation'>
+                    <div className='navigation' ref={menuRef} onClick={toggleMenu}>
                         <div className='menu'>
                             {navLinks.map((item,index) =>(
                                 <NavLink to={item.path} className={navClass => navClass.isActive ? "nav__active nav__item" : "nav__item"} key={index}>{item.display}</NavLink>
