95132cad46c963ea0f487e701838c78bc57ae9e6
PTC-TCOM02\divine.sackitey <divine.sackitey@petraonline.com>
2023-09-10 15:23:11 +0000

    refactor

--- src/App.js
+++ src/App.js
@@ -20,13 +20,3 @@ function App() {
 }
 
 export default App;
-
-
-// {authState ? (
-//   <>
-//     <Route path="/cars" element={<CarListing />} />
-//     <Route path="/comments" element={<Comment />} />
-//   </>
-// ) : (
-//   <Route path="/cars" element={<Navigate to="/login" replace />} />
-// )}
